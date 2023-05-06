a5da42f645e79dfc71a759a01422cc2c7d9e81d3
author <author@fourf>
2023-05-06 16:09:45 +0800

    feat: hook supports get remark

--- src/pages/api/wepaynotify.ts
+++ src/pages/api/wepaynotify.ts
@@ -5,20 +5,23 @@ const manualKey = import.meta.env.MANUAL_KEY;
 const homepage = import.meta.env.PUBLIC_HOMEPAGE;
 const testingEmail: string = import.meta.env.TESTING_EMAIL;
 
-function core(email: string) {
+function core(email: string, remark: string) {
   const approval = `${homepage}api/manual?Token=${manualKey}&Email=${email}`;
 
-  const msg = `User ${email} is going to pay, at ${new Date().toLocaleDateString()}, click ${approval}  to confirm it with your admin account ${testingEmail}`;
+  const msg = `User ${email} is going to pay, remark is ${remark} ,at ${new Date().toLocaleDateString()}, click ${approval}  to confirm it with your admin account ${testingEmail}`;
   botMsg(msg);
 }
 export const post: APIRoute = async ({ request }) => {
-  const { email } = await request.json();
-  core(email);
+  const { email, user, remark } = await request.json();
+  core(email || user, remark);
   return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
 };
 
 export const get: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
-  core(url.searchParams.get('email'));
+  const email = url.searchParams.get('email') || url.searchParams.get('user');
+  const remark = url.searchParams.get('remark');
+
+  core(email, remark);
   return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
 };
