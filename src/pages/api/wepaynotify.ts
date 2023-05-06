d5c2ae3f740da44839f6f8328e6b4bcc3ebbb94e
author <author@fourf>
2023-05-06 16:35:45 +0800

    feat: hook supports get extra

--- src/pages/api/wepaynotify.ts
+++ src/pages/api/wepaynotify.ts
@@ -5,15 +5,15 @@ const manualKey = import.meta.env.MANUAL_KEY;
 const homepage = import.meta.env.PUBLIC_HOMEPAGE;
 const testingEmail: string = import.meta.env.TESTING_EMAIL;
 
-function core(email: string, remark: string) {
-  const approval = `${homepage}api/manual?Token=${manualKey}&Email=${email}`;
+function core(email: string, remark: string, extra: string) {
+  const approval = `${homepage}api/manual?Token=${manualKey}&Email=${email}&Product=${extra}`;
 
-  const msg = `User ${email} is going to pay, remark is ${remark} ,at ${new Date().toLocaleDateString()}, click ${approval}  to confirm it with your admin account ${testingEmail}`;
+  const msg = `User ${email} is going to pay, remark is ${remark}, extra is ${extra} ,at ${new Date().toLocaleString()}, click ${approval}  to confirm it with your admin account ${testingEmail}`;
   botMsg(msg);
 }
 export const post: APIRoute = async ({ request }) => {
-  const { email, user, remark } = await request.json();
-  core(email || user, remark);
+  const { email, user, remark, extra } = await request.json();
+  core(email || user, remark, extra);
   return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
 };
 
@@ -22,7 +22,8 @@ export const get: APIRoute = async ({ request }) => {
   const email = url.searchParams.get('email');
   const user = url.searchParams.get('user');
   const remark = url.searchParams.get('remark');
+  const extra = url.searchParams.get('extra');
 
-  core(email || user, remark);
+  core(email || user, remark, extra);
   return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
 };
