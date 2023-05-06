b39ea9d237ac7f1c376f371fdc21ae6b089368da
author <author@fourf>
2023-05-06 15:53:31 +0800

    feat: hook supports get

--- src/pages/api/wepaynotify.ts
+++ src/pages/api/wepaynotify.ts
@@ -5,12 +5,20 @@ const manualKey = import.meta.env.MANUAL_KEY;
 const homepage = import.meta.env.PUBLIC_HOMEPAGE;
 const testingEmail: string = import.meta.env.TESTING_EMAIL;
 
-export const post: APIRoute = async ({ request }) => {
-  const { email } = await request.json();
+function core(email: string) {
   const approval = `${homepage}api/manual?Token=${manualKey}&Email=${email}`;
 
   const msg = `User ${email} is going to pay, at ${new Date().toLocaleDateString()}, click ${approval}  to confirm it with your admin account ${testingEmail}`;
   botMsg(msg);
+}
+export const post: APIRoute = async ({ request }) => {
+  const { email } = await request.json();
+  core(email);
+  return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
+};
 
-  return new Response(JSON.stringify({ msg }), { status: 200 });
+export const get: APIRoute = async ({ request }) => {
+  const url = new URL(request.url);
+  core(url.searchParams.get('email'));
+  return new Response(JSON.stringify({ msg: 'ok' }), { status: 200 });
 };
